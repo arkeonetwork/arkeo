cd87f78b0043cde33b136ce7d14c38299a2b3220
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2022-11-14 14:35:32 -0500

    fix linting

--- scripts/protoc-swagger-gen.sh
+++ scripts/protoc-swagger-gen.sh
@@ -2,20 +2,20 @@
 
 set -eo pipefail
 
-if ! [ -x "$(command -v swagger-combine )" ]; then
-  echo 'Error: swagger-combine is not installed. Install with $~ npm i -g swagger-combine' >&2
-  exit 1
+if ! [ -x "$(command -v swagger-combine)" ]; then
+	echo 'Error: swagger-combine is not installed. Install with $~ npm i -g swagger-combine' >&2
+	exit 1
 fi
 
 mkdir -p ./tmp-swagger-gen
 cd proto
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  # generate swagger files (filter query files)
-  query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
-  if [[ ! -z "$query_file" ]]; then
-    buf generate --template buf.gen.swagger.yaml $query_file
-  fi
+	# generate swagger files (filter query files)
+	query_file=$(find "${dir}" -maxdepth 1 \( -name 'query.proto' -o -name 'service.proto' \))
+	if [[ -n $query_file ]]; then
+		buf generate --template buf.gen.swagger.yaml "$query_file"
+	fi
 done
 
 cd ..
--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -5,11 +5,11 @@ set -eo pipefail
 cd proto
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 for dir in $proto_dirs; do
-  for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
-    if grep go_package $file &> /dev/null ; then
-      buf generate --template buf.gen.gogo.yaml $file
-    fi
-  done
+	for file in $(find "${dir}" -maxdepth 1 -name '*.proto'); do
+		if grep go_package "$file" &>/dev/null; then
+			buf generate --template buf.gen.gogo.yaml "$file"
+		fi
+	done
 done
 
 cd ..
