0b0ad368b8336231e78b27ddfc6f451f2c2fd0b1
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2022-11-14 20:55:48 -0500

    tweak

--- scripts/genesis.sh
+++ scripts/genesis.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
 set -o pipefail
+set -x
 
 CHAIN_ID="arkeo"
 STAKE="100000000000ukeo"
+TOKEN="ukeo"
 USER="ark"
+
+add_account() {
+  jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.auth.accounts += [{
+        "@type": "/cosmos.auth.v1beta1.BaseAccount",
+        "address": $ADDRESS,
+        "pub_key": null,
+        "account_number": "0",
+        "sequence": "0"
+    }]' <~/.arkeo/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.arkeo/config/genesis.json
+
+  jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.bank.balances += [{
+        "address": $ADDRESS,
+        "coins": [ { "denom": $ASSET, "amount": $AMOUNT } ],
+    }]' <~/.arkeo/config/genesis.json >/tmp/genesis.json
+  mv /tmp/genesis.json ~/.arkeo/config/genesis.json
+}
 
 if [ ! -f ~/.arkeo/config/priv_validator_key.json ]; then
 	# remove the original generate genesis file, as below will init chain again
@@ -12,16 +31,26 @@ if [ ! -f ~/.arkeo/config/priv_validator_key.json ]; then
 fi
 
 if [ ! -f ~/.arkeo/config/genesis.json ]; then
-	arkeod init local --chain-id "$CHAIN_ID"
+	arkeod init local --staking-bond-denom $TOKEN --chain-id "$CHAIN_ID"
 	arkeod keys add $USER --keyring-backend test
 	arkeod add-genesis-account $USER $STAKE --keyring-backend test
 	arkeod keys list --keyring-backend test
 	arkeod gentx $USER $STAKE --chain-id $CHAIN_ID --keyring-backend test
 	arkeod collect-gentxs
 
-	arkeod validate-genesis --trace
+    if [ "$NET" = "mocknet" ] || [ "$NET" = "testnet" ]; then
+        add_account rko1dheycdevq39qlkxs2a6wuuzyn4aqxhvee2kjas ukeo 10000000000000000 # reserve, 100m
+
+        arkeod keys add faucet --keyring-backend test
+        FAUCET=$(arkeod keys show faucet -a)
+        add_account $FAUCET ukeo 10000000000000000 # faucet, 100m
+    fi
+
+	sed -i 's/"stake"/"ukeo"/g' ~/.arkeo/config/genesis.json
 	sed -i 's/enable = false/enable = true/g' ~/.arkeo/config/app.toml
 	sed -i 's/127.0.0.1:26657/0.0.0.0:26657/g' ~/.arkeo/config/config.toml
+
+	arkeod validate-genesis --trace
 fi
 
 arkeod start
