007efa27aec74c7f3eeff1b03785927fae5d07c3
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2022-11-14 21:08:32 -0500

    linting fixes

--- scripts/genesis.sh
+++ scripts/genesis.sh
@@ -9,20 +9,20 @@ TOKEN="ukeo"
 USER="ark"
 
 add_account() {
-  jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.auth.accounts += [{
+	jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.auth.accounts += [{
         "@type": "/cosmos.auth.v1beta1.BaseAccount",
         "address": $ADDRESS,
         "pub_key": null,
         "account_number": "0",
         "sequence": "0"
     }]' <~/.arkeo/config/genesis.json >/tmp/genesis.json
-  mv /tmp/genesis.json ~/.arkeo/config/genesis.json
+	mv /tmp/genesis.json ~/.arkeo/config/genesis.json
 
-  jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.bank.balances += [{
+	jq --arg ADDRESS "$1" --arg ASSET "$2" --arg AMOUNT "$3" '.app_state.bank.balances += [{
         "address": $ADDRESS,
         "coins": [ { "denom": $ASSET, "amount": $AMOUNT } ],
     }]' <~/.arkeo/config/genesis.json >/tmp/genesis.json
-  mv /tmp/genesis.json ~/.arkeo/config/genesis.json
+	mv /tmp/genesis.json ~/.arkeo/config/genesis.json
 }
 
 if [ ! -f ~/.arkeo/config/priv_validator_key.json ]; then
@@ -38,13 +38,13 @@ if [ ! -f ~/.arkeo/config/genesis.json ]; then
 	arkeod gentx $USER $STAKE --chain-id $CHAIN_ID --keyring-backend test
 	arkeod collect-gentxs
 
-    if [ "$NET" = "mocknet" ] || [ "$NET" = "testnet" ]; then
-        add_account rko1dheycdevq39qlkxs2a6wuuzyn4aqxhvee2kjas ukeo 10000000000000000 # reserve, 100m
+	if [ "$NET" = "mocknet" ] || [ "$NET" = "testnet" ]; then
+		add_account rko1dheycdevq39qlkxs2a6wuuzyn4aqxhvee2kjas ukeo 10000000000000000 # reserve, 100m
 
-        arkeod keys add faucet --keyring-backend test
-        FAUCET=$(arkeod keys show faucet -a)
-        add_account $FAUCET ukeo 10000000000000000 # faucet, 100m
-    fi
+		arkeod keys add faucet --keyring-backend test
+		FAUCET=$(arkeod keys show faucet -a)
+		add_account "$FAUCET" ukeo 10000000000000000 # faucet, 100m
+	fi
 
 	sed -i 's/"stake"/"ukeo"/g' ~/.arkeo/config/genesis.json
 	sed -i 's/enable = false/enable = true/g' ~/.arkeo/config/app.toml
