2c2c8b47c3e1b25ead1510da08daea2beb210f11
Son of Odin <2700975-son-of-odin@users.noreply.gitlab.com>
2022-12-13 17:49:41 -0500

    add username/password

--- sentinel/sentinel.go
+++ sentinel/sentinel.go
@@ -42,10 +42,66 @@ func (p Proxy) serveReverseProxy(w http.ResponseWriter, r *http.Request, host st
 	fmt.Println("Proxy Redirect:", url)
 
 	// create the reverse proxy
-	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy := NewSingleHostReverseProxy(url)
 
 	// Note that ServeHttp is non blocking and uses a go routine under the hood
 	proxy.ServeHTTP(w, r)
+}
+
+func NewSingleHostReverseProxy(target *url.URL) *httputil.ReverseProxy {
+	targetQuery := target.RawQuery
+	director := func(req *http.Request) {
+		req.URL.Scheme = target.Scheme
+		req.URL.Host = target.Host
+		req.URL.Path, req.URL.RawPath = joinURLPath(target, req.URL)
+		if targetQuery == "" || req.URL.RawQuery == "" {
+			req.URL.RawQuery = targetQuery + req.URL.RawQuery
+		} else {
+			req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
+		}
+		if _, ok := req.Header["User-Agent"]; !ok {
+			// explicitly disable User-Agent so it's not set to default value
+			req.Header.Set("User-Agent", "")
+		}
+		passwd, ok := req.URL.User.Password()
+		if ok {
+			req.SetBasicAuth(req.URL.User.Username(), passwd)
+		}
+	}
+	return &httputil.ReverseProxy{Director: director}
+}
+
+func joinURLPath(a, b *url.URL) (path, rawpath string) {
+	if a.RawPath == "" && b.RawPath == "" {
+		return singleJoiningSlash(a.Path, b.Path), ""
+	}
+	// Same as singleJoiningSlash, but uses EscapedPath to determine
+	// whether a slash should be added
+	apath := a.EscapedPath()
+	bpath := b.EscapedPath()
+
+	aslash := strings.HasSuffix(apath, "/")
+	bslash := strings.HasPrefix(bpath, "/")
+
+	switch {
+	case aslash && bslash:
+		return a.Path + b.Path[1:], apath + bpath[1:]
+	case !aslash && !bslash:
+		return a.Path + "/" + b.Path, apath + "/" + bpath
+	}
+	return a.Path + b.Path, apath + bpath
+}
+
+func singleJoiningSlash(a, b string) string {
+	aslash := strings.HasSuffix(a, "/")
+	bslash := strings.HasPrefix(b, "/")
+	switch {
+	case aslash && bslash:
+		return a + b[1:]
+	case !aslash && !bslash:
+		return a + "/" + b
+	}
+	return a + b
 }
 
 // Given a request send it to the appropriate url
@@ -63,7 +119,7 @@ func (p Proxy) handleRequestAndRedirect(w http.ResponseWriter, r *http.Request) 
 	switch host { // nolint
 	case "btc-mainnet-fullnode":
 		// add username/password to request
-		host = fmt.Sprintf("%s:8332", host)
+		host = fmt.Sprintf("thorchain:password@%s:8332", host)
 		r.URL.User = url.UserPassword("thorchain", "password")
 	}
 
