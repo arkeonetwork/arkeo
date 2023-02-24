89c79dc5e73d7041bfa20bb4ba8f6bdd0788f208
Johnny Luo <johnnyluo1980@gmail.com>
2023-02-24 16:58:46 +1100

    Fix format

--- x/claim/client/cli/tx_add_claim.go
+++ x/claim/client/cli/tx_add_claim.go
@@ -4,13 +4,10 @@ package cli
 
 import (
 	"fmt"
-	"strconv"
 
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 )
-
-var _ = strconv.Itoa(0)
 
 func CmdAddClaim() *cobra.Command {
 	cmd := &cobra.Command{
