6f65b176ecce722a67eee3617ac27b1c903a84f0
0xean <0xean.eth@gmail.com>
2023-02-27 18:11:53 -0500

    re-enumerate

--- x/claim/types/errors.go
+++ x/claim/types/errors.go
@@ -5,8 +5,8 @@ import (
 )
 
 var (
-	ErrAirdropEnded                = errors.Register(ModuleName, 2, "Airdrop has ended")
-	ErrNoClaimableAmount           = errors.Register(ModuleName, 3, "No Claimable Arkeo")
-	ErrInvalidSignature            = errors.Register(ModuleName, 4, "Invalid signature")
-	ErrClaimRecordNotTransferrable = errors.Register(ModuleName, 5, "Claim record can not be transferred")
+	ErrAirdropEnded                = errors.Register(ModuleName, 1, "Airdrop has ended")
+	ErrNoClaimableAmount           = errors.Register(ModuleName, 2, "No Claimable Arkeo")
+	ErrInvalidSignature            = errors.Register(ModuleName, 3, "Invalid signature")
+	ErrClaimRecordNotTransferrable = errors.Register(ModuleName, 4, "Claim record can not be transferred")
 )
