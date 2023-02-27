ede2ac0e60adc10027472c9e549338f1f8826a00
0xean <0xean.eth@gmail.com>
2023-02-27 12:25:48 -0700

    fix spelling

--- x/claim/types/errors.go
+++ x/claim/types/errors.go
@@ -8,5 +8,5 @@ var (
 	ErrAirdropEnded                = errors.Register(ModuleName, 2, "Airdrop has ended")
 	ErrNoClaimableAmount           = errors.Register(ModuleName, 3, "No Claimable Arkeo")
 	ErrInvalidSignature            = errors.Register(ModuleName, 4, "Invalid signature")
-	ErrClaimRecordNotTransferrable = errors.Register(ModuleName, 5, "Claim record can not be transfered")
+	ErrClaimRecordNotTransferrable = errors.Register(ModuleName, 5, "Claim record can not be transferred")
 )
