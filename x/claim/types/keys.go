4a761bba4a3de06f9cfa4b819b83f28cef9a2c67
Johnny Luo <johnnyluo1980@gmail.com>
2023-03-02 10:22:48 +1100

    revert key change

--- x/claim/types/keys.go
+++ x/claim/types/keys.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	// ModuleName defines the module name
-	ModuleName = "claim-arkeo" // append arkeo to avoid namespace collisions with comsos claim module
+	ModuleName = "arkeo-claim" // preappend arkeo to avoid namespace collisions with comsos claim module
 
 	// StoreKey defines the primary module store key
 	StoreKey = ModuleName
