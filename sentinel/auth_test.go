b21c252f3267892506533160e080b7709bc6fd4a
Johnny Luo <johnnyluo1980@gmail.com>
2023-03-03 12:58:35 +1100

    Fall back to free tier when paid tier failed

--- sentinel/auth_test.go
+++ sentinel/auth_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"time"
 
 	"github.com/arkeonetwork/arkeo/common"
@@ -150,4 +151,70 @@ func (s *AuthSuite) TestPaidTier(c *C) {
 	code, err = proxy.paidTier(aa, "127.0.0.1:8080")
 	c.Assert(err, NotNil)
 	c.Check(code, Equals, http.StatusTooManyRequests)
+}
+
+func (s *AuthSuite) TestPaidTierFailFallbackToFreeTier(c *C) {
+	// setup
+	interfaceRegistry := codectypes.NewInterfaceRegistry()
+	std.RegisterInterfaces(interfaceRegistry)
+	module.NewBasicManager().RegisterInterfaces(interfaceRegistry)
+	ctypes.RegisterInterfaces(interfaceRegistry)
+	cdc := codec.NewProtoCodec(interfaceRegistry)
+
+	visitors = make(map[string]*rate.Limiter) // reset visitors
+	pubkey := types.GetRandomPubKey()
+	kb := cKeys.NewInMemory(cdc)
+	info, _, err := kb.NewMnemonic("whatever", cKeys.English, `m/44'/931'/0'/0/0`, "", hd.Secp256k1)
+	c.Assert(err, IsNil)
+	pub, err := info.GetPubKey()
+	c.Assert(err, IsNil)
+	pk, err := common.NewPubKeyFromCrypto(pub)
+	c.Assert(err, IsNil)
+
+	var signature []byte
+	height := int64(12)
+	nonce := int64(3)
+	chain := common.BTCChain.String()
+
+	message := []byte(fmt.Sprintf("%s:%s:%s:%d:%d", pubkey.String(), chain, pk, height, nonce))
+	signature, _, err = kb.Sign("whatever", message)
+	c.Assert(err, IsNil)
+
+	config := conf.Configuration{
+		ProviderPubKey:            pubkey,
+		AsGoTierRateLimitDuration: time.Minute,
+		AsGoTierRateLimit:         1,
+		SubTierRateLimitDuration:  time.Minute,
+		SubTierRateLimit:          1,
+		FreeTierRateLimitDuration: time.Minute,
+		FreeTierRateLimit:         1,
+	}
+	proxy := NewProxy(config)
+
+	contract := types.NewContract(pubkey, common.BTCChain, pk)
+	contract.Height = 5
+	contract.Duration = 100
+	// set contract to expired
+	proxy.MemStore.SetHeight(120)
+	key := proxy.MemStore.Key(pubkey.String(), common.BTCChain.String(), pk.String())
+	proxy.MemStore.Put(key, contract)
+
+	nextHandler := http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+	})
+	handlerForTest := proxy.auth(nextHandler)
+	c.Assert(handlerForTest, NotNil)
+	aa := ArkAuth{
+		Provider:  pubkey,
+		Height:    height,
+		Nonce:     nonce,
+		Chain:     common.BTCChain,
+		Spender:   pk,
+		Signature: signature,
+	}
+
+	responseRecorder := httptest.NewRecorder()
+	target := "/test?arkauth=" + aa.String()
+	r := httptest.NewRequest(http.MethodPost, target, nil)
+	handlerForTest.ServeHTTP(responseRecorder, r)
+	c.Assert(responseRecorder.Code, Equals, 200)
 }
