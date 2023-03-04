1fad196162cd42ef0e70d4af8cc973c8643f7b6d
0xean <0xean.eth@gmail.com>
2023-03-04 16:47:19 -0700

    adds tests for closing pay as you go contracts

--- x/arkeo/keeper/msg_server_close_contract_test.go
+++ x/arkeo/keeper/msg_server_close_contract_test.go
@@ -173,3 +173,87 @@ func (CloseContractSuite) TestCloseSubscriptionContract(c *C) {
 	_, err = s.CloseContract(ctx, &closeContractMsg)
 	c.Check(err, IsNil)
 }
+
+func (CloseContractSuite) TestClosePayAsYouGoContract(c *C) {
+	// NOTE: pay as you go contracts cannot be closed on demand.
+	ctx, k, sk := SetupKeeperWithStaking(c)
+	ctx = ctx.WithBlockHeight(10)
+	s := newMsgServer(k, sk)
+
+	// set up provider
+	providerPubKey := types.GetRandomPubKey()
+	providerAddress, err := providerPubKey.GetMyAddress()
+	c.Assert(err, IsNil)
+	chain := common.BTCChain
+	provider := types.NewProvider(providerPubKey, chain)
+	provider.Bond = cosmos.NewInt(10000000000)
+	c.Assert(k.SetProvider(ctx, provider), IsNil)
+
+	modProviderMsg := types.MsgModProvider{
+		Provider:            provider.PubKey,
+		Chain:               provider.Chain.String(),
+		MinContractDuration: 10,
+		MaxContractDuration: 500,
+		Status:              types.ProviderStatus_ONLINE,
+		PayAsYouGoRate:      15,
+		SubscriptionRate:    15,
+	}
+	err = s.ModProviderHandle(ctx, &modProviderMsg)
+
+	c.Assert(err, IsNil)
+	c.Assert(k.MintAndSendToAccount(ctx, providerAddress, getCoin(common.Tokens(10))), IsNil)
+
+	// set up contract with no delegate address
+	userPubKey := types.GetRandomPubKey()
+	userAddress, err := userPubKey.GetMyAddress()
+	c.Assert(err, IsNil)
+
+	openContractMessage := types.MsgOpenContract{
+		Provider:     providerPubKey,
+		Chain:        chain.String(),
+		Creator:      providerAddress.String(),
+		Client:       userPubKey,
+		ContractType: types.ContractType_PAY_AS_YOU_GO,
+		Duration:     100,
+		Rate:         15,
+		Deposit:      cosmos.NewInt(1500),
+	}
+	_, err = s.OpenContract(ctx, &openContractMessage)
+	contract, err := s.GetActiveContractForUser(ctx, userPubKey, providerPubKey, chain)
+	c.Assert(err, IsNil)
+
+	// confirm that another user cannot close the contract
+	use2PubKey := types.GetRandomPubKey()
+	user2Address, err := use2PubKey.GetMyAddress()
+	c.Assert(err, IsNil)
+
+	closeContractMsg := types.MsgCloseContract{
+		Creator:    user2Address.String(),
+		ContractId: contract.Id,
+	}
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, ErrIs, types.ErrCloseContractUnauthorized)
+
+	// confirm that the contract can not be closed by the client
+	closeContractMsg.Creator = userAddress.String()
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, ErrIs, types.ErrCloseContractUnauthorized)
+
+	// reopen contract this time with a delagate address.
+	openContractMessage.Delegate = use2PubKey
+	_, err = s.OpenContract(ctx, &openContractMessage)
+	c.Assert(err, IsNil)
+
+	contract, err = s.GetActiveContractForUser(ctx, userPubKey, providerPubKey, chain)
+	closeContractMsg.ContractId = contract.Id
+
+	// confirm that the contract cannot be closed by the delegate
+	closeContractMsg.Creator = user2Address.String()
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, ErrIs, types.ErrCloseContractUnauthorized)
+
+	// nor the client
+	closeContractMsg.Creator = userAddress.String()
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, ErrIs, types.ErrCloseContractUnauthorized)
+}
