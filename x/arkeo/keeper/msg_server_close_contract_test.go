096a23cbebf16119bda0f7d664d08419d09d0a01
0xean <0xean.eth@gmail.com>
2023-03-04 16:43:00 -0700

    add more extensive tests for subscription contract types

--- x/arkeo/keeper/msg_server_close_contract_test.go
+++ x/arkeo/keeper/msg_server_close_contract_test.go
@@ -91,42 +91,85 @@ func (CloseContractSuite) TestHandle(c *C) {
 	c.Check(bal.Int64(), Equals, int64(2))
 }
 
-func (CloseContractSuite) TestCloseContract(c *C) {
+func (CloseContractSuite) TestCloseSubscriptionContract(c *C) {
 	ctx, k, sk := SetupKeeperWithStaking(c)
-	ctx = ctx.WithBlockHeight(14)
-
+	ctx = ctx.WithBlockHeight(10)
 	s := newMsgServer(k, sk)
 
-	// setup
-	c.Assert(k.MintToModule(ctx, types.ModuleName, getCoin(500)), IsNil)
-	c.Assert(k.SendFromModuleToModule(ctx, types.ModuleName, types.ContractName, getCoins(500)), IsNil)
-	pubkey := types.GetRandomPubKey()
-	provider, err := pubkey.GetMyAddress()
+	// set up provider
+	providerPubKey := types.GetRandomPubKey()
+	providerAddress, err := providerPubKey.GetMyAddress()
 	c.Assert(err, IsNil)
-	acc := types.GetRandomPubKey()
 	chain := common.BTCChain
-	c.Check(k.GetBalance(ctx, provider).IsZero(), Equals, true)
+	provider := types.NewProvider(providerPubKey, chain)
+	provider.Bond = cosmos.NewInt(10000000000)
+	c.Assert(k.SetProvider(ctx, provider), IsNil)
 
-	contract := types.NewContract(pubkey, chain, acc)
-	contract.Type = types.ContractType_SUBSCRIPTION
-	contract.Duration = 100
-	contract.Height = 10
-	contract.Rate = 5
-	contract.Deposit = cosmos.NewInt(500)
-	contract.Id = 1
+	modProviderMsg := types.MsgModProvider{
+		Provider:            provider.PubKey,
+		Chain:               provider.Chain.String(),
+		MinContractDuration: 10,
+		MaxContractDuration: 500,
+		Status:              types.ProviderStatus_ONLINE,
+		PayAsYouGoRate:      15,
+		SubscriptionRate:    15,
+	}
+	err = s.ModProviderHandle(ctx, &modProviderMsg)
 
-	// openContractMsg =: types.MsgOpenContract {
+	c.Assert(err, IsNil)
+	c.Assert(k.MintAndSendToAccount(ctx, providerAddress, getCoin(common.Tokens(10))), IsNil)
 
-	// }
+	// set up contract with no delegate address
+	userPubKey := types.GetRandomPubKey()
+	userAddress, err := userPubKey.GetMyAddress()
+	c.Assert(err, IsNil)
 
-	// s.OpenContract(ctx)
+	openContractMessage := types.MsgOpenContract{
+		Provider:     providerPubKey,
+		Chain:        chain.String(),
+		Creator:      providerAddress.String(),
+		Client:       userPubKey,
+		ContractType: types.ContractType_SUBSCRIPTION,
+		Duration:     100,
+		Rate:         15,
+		Deposit:      cosmos.NewInt(1500),
+	}
+	_, err = s.OpenContract(ctx, &openContractMessage)
+	contract, err := s.GetActiveContractForUser(ctx, userPubKey, providerPubKey, chain)
+	c.Assert(err, IsNil)
 
-	c.Assert(k.SetContract(ctx, contract), IsNil)
+	// confirm that another user cannot close the contract
+	use2PubKey := types.GetRandomPubKey()
+	user2Address, err := use2PubKey.GetMyAddress()
+	c.Assert(err, IsNil)
 
-	// happy path
-	msg := types.MsgCloseContract{
-		Creator:    acc.String(),
+	closeContractMsg := types.MsgCloseContract{
+		Creator:    user2Address.String(),
 		ContractId: contract.Id,
 	}
-	c.Assert(s.CloseContractHandle(ctx, &msg), IsNil)
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, ErrIs, types.ErrCloseContractUnauthorized)
+
+	// confirm that the contract can be closed by the client
+	closeContractMsg.Creator = userAddress.String()
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, IsNil)
+
+	// reopen contract this time with a delagate address.
+	openContractMessage.Delegate = use2PubKey
+	_, err = s.OpenContract(ctx, &openContractMessage)
+	c.Assert(err, IsNil)
+
+	contract, err = s.GetActiveContractForUser(ctx, userPubKey, providerPubKey, chain)
+	closeContractMsg.ContractId = contract.Id
+
+	// confirm that the contract cannot be closed by the delegate
+	closeContractMsg.Creator = user2Address.String()
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, ErrIs, types.ErrCloseContractUnauthorized)
+
+	// but can be closed by the client
+	closeContractMsg.Creator = userAddress.String()
+	_, err = s.CloseContract(ctx, &closeContractMsg)
+	c.Check(err, IsNil)
 }
