32a870506fe773ce880d67e6f9ddbfc5470fb564
toshiSat <10103480+toshiSat@users.noreply.github.com>
2024-03-25 12:45:17 -0600

    try to fix lint

--- test/regression/cmd/config.go
+++ test/regression/cmd/config.go
@@ -136,8 +136,6 @@ var httpClient = &http.Client{
 ////////////////////////////////////////////////////////////////////////////////////////
 
 // trunk-ignore-all(gitleaks/generic-api-key)
-// trunk-ignore(trunk/ignore-does-nothing) # Getting weird lint error only on CI and running locally works fine
-// trunk-ignore-all(golangci-lint/gosec)
 const (
 	ModuleAddrBondedTokensPool    = "tarkeo1fl48vsnmsdzcv85q5d2q4z5ajdha8yu3e79s43"
 	ModuleAddrNotBondedTokensPool = "tarkeo1tygms3xhhs3yv487phx3dw4a95jn7t7ld7epr9"
