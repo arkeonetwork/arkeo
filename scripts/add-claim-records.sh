4706d64a3679f50fd82ccc1d4e04fcfff898c2bd
toshiSat <10103480+toshiSat@users.noreply.github.com>
2024-12-05 20:33:56 +0900

    Add final csv's

--- scripts/add-claim-records.sh
+++ scripts/add-claim-records.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Input files and genesis file
-CHAIN1_CSV="./data/arkeo_airdrop_bech32.csv" # Replace with the actual filename for the first chain
-CHAIN2_CSV="./data/combined_eth_airdrop.csv" # Replace with the actual filename for the second chain
+CHAIN1_CSV="./data/arkeo_airdrop.csv" 
+CHAIN2_CSV="./data/eth_airdrop.csv"
 GENESIS_FILE="~/.arkeo/config/genesis.json"
 TEMP_FILE="/tmp/genesis_temp.json"
 
@@ -11,11 +11,6 @@ if [ ! -f "$CHAIN1_CSV" ] || [ ! -f "$CHAIN2_CSV" ]; then
     echo "CSV file for one of the chains not found!"
     exit 1
 fi
-
-# if [ ! -f "$GENESIS_FILE" ]; then
-#     echo "Genesis file not found!"
-#     exit 1
-# fi
 
 # Function to process a CSV file and add entries to the genesis file
 add_claim_records() {
@@ -27,7 +22,6 @@ add_claim_records() {
 
     # Loop through each line in the CSV file (skip the header)
     while IFS=, read -r address amount; do
-        # Multiply the amount by 10^9
         amount_claim=$(echo "scale=0; $amount * 100000000 / 3" | bc)
         amount_vote=$(echo "scale=0; $amount * 100000000 / 3" | bc)
         amount_delegate=$(echo "scale=0; $amount * 100000000 / 3" | bc)
@@ -38,18 +32,15 @@ add_claim_records() {
         records+=","
     done < <(tail -n +2 "$csv_file")
 
-    # Remove the trailing comma and close the JSON array
     records="${records%,}]"
 
     echo "Final JSON Records Array: $records"
 
-    # Add the batch of records to the genesis.json in one operation
     jq --argjson new_records "$records" '.app_state.claimarkeo.claim_records += $new_records' <~/.arkeo/config/genesis.json >/tmp/genesis.json
     mv /tmp/genesis.json ~/.arkeo/config/genesis.json
 }
 
-# Process CSV files for each chain
 add_claim_records "$CHAIN1_CSV" "ARKEO"
 add_claim_records "$CHAIN2_CSV" "ETHEREUM"
 
-echo "Updated genesis.json with new claim records for both chains."
+echo "Updated genesis.json with new claim records for arkeo and ethereum."
